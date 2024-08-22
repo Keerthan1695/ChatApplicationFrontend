04c756000210d8705f8b71335d322cd053c34408
keerthan <keerthan@keerthans-MacBook-Air.local>
2024-08-22 22:02:47 +0530

    render commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/frontend/"
-})
+  server: {
+    port:3000,
+    host: true
+  },
+  preview: {
+    port:3000,
+    host: true
+  }
+});
